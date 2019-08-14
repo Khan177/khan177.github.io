9ae18bc7006c9a10fd8d41c2ca1e7489c0a64bd6
Adilkhan Nurgaliyev <khan17171717@gmail.com>
2019-08-14 18:52:04 +0600

    link

---
+++ precache-manifest.09934a9f4c3c20c9a61b9dfd907c38d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e9489b63ad5938c48c2",
+    "url": "/static/css/main.82a42c6a.chunk.css"
+  },
+  {
+    "revision": "4e9489b63ad5938c48c2",
+    "url": "/static/js/main.948bc656.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b548e30793887743f0b8",
+    "url": "/static/js/2.cf331bc2.chunk.js"
+  },
+  {
+    "revision": "e06cb725aefcef21cc79f136c96af591",
+    "url": "/index.html"
+  }
+];
